30963c6ae7459a8a9837179b425d0298e46fa7ae
Anthony <nemothetuna@gmail.com>
2022-08-12 20:30:14 -0700

    working on filtering out data

--- src/routes/Store.tsx
+++ src/routes/Store.tsx
@@ -1,14 +1,34 @@
 import './Store.css'
-import { getData } from '../data';
+import { Data, getData } from '../data';
+import { useEffect, useState } from 'react';
 
 export default function Store() {
-  let data = getData();
+  let data: Data[] = getData();
+  const [ filteredData, setFilteredData ] = useState(data);
+
+  const [ itemToFind, setItemToFind ] = useState('');
+
+  function findItem(name: string) {
+    setItemToFind(name);
+  }
+
+  useEffect(() => {
+    console.log(data);
+    if(itemToFind === '') return;
+
+    // setFilteredData(data.filter(item => item.name == itemToFind));
+
+    // let filter = data.find(item => item.name.includes(itemToFind))
+    let filter: Data[] = data.filter(item => item.name.includes(itemToFind))
+    // console.log(filter);
+    setFilteredData(filter);
+  }, [itemToFind]);
 
   return (
     <main>
       <form className='search-bar'>
         <label htmlFor='product_search'>
-          <input type='search' name='product_search' id='product_search' placeholder='Search for product...' />
+          <input onChange={e => findItem(e.target.value)} type='search' name='product_search' id='product_search' placeholder='Search for product...' />
         </label>
       </form>
 
